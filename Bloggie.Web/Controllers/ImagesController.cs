58bcad8affe6f813376301b19dfbbf2f1f4fd09b
Safin-Sarker <safinsarker1122@gmail.com>
2024-03-12 22:39:03 +0100

    Create POST method and Image Repository

---
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            return Ok("This is the GET Images API call");
+        }
+    }
+}
