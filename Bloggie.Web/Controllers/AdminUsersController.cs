b27ed47636d97b2c4189a4a7c03555c8eeb61e62
Safin-Sarker <safinsarker1122@gmail.com>
2024-04-05 19:57:28 +0200

    again

--- Bloggie.Web/Controllers/AdminUsersController.cs
+++ Bloggie.Web/Controllers/AdminUsersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 namespace Bloggie.Web.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")] 
     public class AdminUsersController : Controller
     {
         private readonly IUserRepository userRepository;
